706ecb4a8077a410e32b88f3a164d488521e2499
rehanshahjahan98 <rehan.shahjahan98@gmail.com>
2024-02-19 08:16:21 +0400

    added address

--- CrazyFlip-frontend-main/src/Context/constant.js
+++ CrazyFlip-frontend-main/src/Context/constant.js
@@ -4,7 +4,7 @@ import affiliate from "./affiliate.json";
 import erc20 from "./erc20.json";
 //NFT MARKETPLACE
 export const coinFlipContractAddress =
-	"0x62d033ED8C241A93436d3d1e29B3b6a595D5FEFD";
+	"0xBE8Bd5Ea42b2355d0a674237c396ba00B3Bb0fA5";
 export const affiliateContractAddress =
 	"0xE5f95c1fA96C4baDAD2f279eE8fCF5507F0c9703";
 export const coinFlipABI = coinFlip.abi;
